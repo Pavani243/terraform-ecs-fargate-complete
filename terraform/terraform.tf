638feee37da0e9408c2797093a2bad180a9d2191
Pavani Sapparapu <sapparap@amazon.com>
2023-04-07 12:10:44 -0500

    updated tf version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,12 +6,5 @@ terraform {
     }
   }
 
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.6"
-    }
-  }
+  required_version = ">= 0.12"
 }
